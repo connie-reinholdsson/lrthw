5f2ccf2a635d0b07119b6711757d0add2f493054
connie-reinholdsson <connie.reinholdsson@gmail.com>
2017-02-21 17:11:26 +0000

    Ex13a Answers

---
+++ chap13/ex13a.rb
@@ -0,0 +1,10 @@
+first, second, third = ARGV
+#This document takes three arguments.
+
+puts "Your variable is #{first}."
+
+puts "Your variable is #{second}."
+
+puts "Your variable is #{third}."
+
+#Line 1 unpacks ARGV, so that it is assigned to three variables: first, second, third.
