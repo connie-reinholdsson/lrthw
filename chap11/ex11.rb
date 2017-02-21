689301cc8eba291969cbe3b20149e1883f9f6da6
connie-reinholdsson <connie.reinholdsson@gmail.com>
2017-02-21 15:31:06 +0000

    Ex11 Answers

---
+++ chap11/ex11.rb
@@ -0,0 +1,26 @@
+print "How old are you?"
+age = gets.chomp
+
+print "How tall are you?"
+height = gets.chomp
+
+print "How much do you weigh?"
+weight = gets.chomp
+
+#Note: Print is used instead of puts.
+#Resulting in the answer / input being printed after the question.
+
+#Study Drills Q3
+
+puts "So you're #{age} years old, #{height} tall and #{weight} heavy."
+
+print "What's your name?"
+name = gets.chomp
+
+print "Where do you live?"
+place = gets.chomp
+
+print "What's your favourite programming language?"
+language = gets.chomp
+
+puts "So you're name is #{name}, you live in #{place} and your favourite programming language is #{language}!"
