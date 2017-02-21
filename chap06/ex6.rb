54af011c4a09bcb8da969edb52ac499411b10030
connie-reinholdsson <connie.reinholdsson@gmail.com>
2017-02-21 14:37:26 +0000

    Ex6 Answers

---
+++ chap06/ex6.rb
@@ -0,0 +1,21 @@
+types_of_people = 10
+x = "There are #{types_of_people} types of people."
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+
+puts x #"There are 10 types of people."
+puts y #Those who know binary and those who don't.
+
+puts "I said: #{x}." #I said: There are 10 types of people.
+puts " I also said: '#{y}'." #I also said: y..
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny? #{hilarious}"
+
+puts joke_evaluation #false
+
+w = 'This is the left side of...'
+e = "a string with a right side."
+
+puts w + e #This is the left side of a string with a right side.
