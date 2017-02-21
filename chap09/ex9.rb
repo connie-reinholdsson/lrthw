ed180451e239500ebae047b597e1880bb7341d0d
connie-reinholdsson <connie.reinholdsson@gmail.com>
2017-02-21 14:53:57 +0000

    Ex9 Answers

---
+++ chap09/ex9.rb
@@ -0,0 +1,13 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+#\n adds a new line before the new word, not after! See result of Jan example in terminal.
+
+puts "Here are the days: #{days}."
+puts "Here are the months #{months}."
+
+puts """
+When I write with three double-quotes...
+I can write lots of new lines...
+with no errors!
+"""
