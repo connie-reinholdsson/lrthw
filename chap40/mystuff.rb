ad1db169f7850932bb44f1d2bac0bd2b8e86f949
connie-reinholdsson <connie.reinholdsson@gmail.com>
2017-02-23 18:03:46 +0000

    Module MyStuff

---
+++ chap40/mystuff.rb
@@ -0,0 +1,9 @@
+#New module.. mystuff.rb
+module MyStuff
+  def MyStuff.apple()
+    puts "I AM APPLES!"
+  end
+
+#this is just a variable.. not sure why capital!
+TANGERINE = "Living reflection of a dream"
+end
