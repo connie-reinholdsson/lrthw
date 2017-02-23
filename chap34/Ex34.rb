74e0b2ad4c0615b7bc25f754fe93504d8b40ed82
connie-reinholdsson <connie.reinholdsson@gmail.com>
2017-02-23 10:49:57 +0000

    Ex34 Answers

---
+++ chap34/Ex34.rb
@@ -0,0 +1,11 @@
+training = ["Yoga", "HIIT", "Running", "Walking", "Spinning"]
+
+puts training[0] #Yoga
+puts training[3] #Walking
+puts training[4] #Spinning
+
+foods = ["Pizza", "Burrito", "Nachos", "Smoothie"]
+
+puts foods[3] #Smoothie
+puts foods[1] #Burrito
+puts foods[2] #Nachos
