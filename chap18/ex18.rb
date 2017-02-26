82d130c575cab283f6c0e99ade8ccd028178f63e
connie-reinholdsson <connie.reinholdsson@gmail.com>
2017-02-26 17:13:04 +0000

    Ex18 Answers

--- chap18/ex18.rb
+++ chap18/ex18.rb
@@ -1,29 +1,34 @@
 #FUNCTIONS
+
+# this one is like our script with argv
 def print_two(*args)
   arg1, arg2 = args
   puts "arg1: #{arg1}, arg2: #{arg2}"
 end
-#This function prints arg1, arg2.
 
-
+# *args is pointless, we can do this instead
 def print_two_again(arg1, arg2)
   puts "arg1: #{arg1}, arg2: #{arg2}"
 end
-#This is an improved version, with two arguments specified.
 
+# this takes one argument
 def print_one(arg1)
   puts "arg1: #{arg1}"
 end
-#This version takes one argument.
 
+#this one takes no arguments
 def print_none()
 puts "I've got nothing!"
 end
-#This function takes no arguments.
 
 print_two("Connie", "Reinholdsson")
+#call the function with two arguments.
 print_two_again("Connie", "Reinholdsson")
-
+#call the function with two arguments.
 print_one("One argument over here!")
+#call the function with one argument.
 print_none()
+#call the function with no arguments.
 #We don't actually need the brackets, but use them for demonstration.
+
+#Remember: To 'run', 'call' or 'use' a function all mean the same thing!
