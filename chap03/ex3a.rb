fd0e43a12c2330ff69d0dc203a80f6d1908aa078
connie-reinholdsson <connie.reinholdsson@gmail.com>
2017-02-26 12:35:38 +0000

    Ex3a Answers

---
+++ chap03/ex3a.rb
@@ -0,0 +1,24 @@
+puts "I will now count my chickens:"
+#Prints it to the screen followed by space.
+puts "Hens #{25.0 + 30.0 / 6.0}"
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+#Calculates the numbers within the #{} and prints the result to the screen as floats.
+puts "I will now count the eggs:"
+#Prints string to the screen followed by space.
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+#Calculates the numbers and prints the result to the screen as a float.
+puts "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
+#Prints the string to the screen. The numbers are part of the string so no calculations are made.
+puts 3.0 + 2.0 < 5.0 - 7.0
+#Calculates the numbers and prints the result to the screen as a float.
+puts "What is 3.0 + 2.0? #{3.0 + 2.0}"
+puts "What is 5.0 - 7.0? #{5.0 - 7.0}"
+#Prints the strings followed by the calculated floats within the #{}.
+puts "Oh, that's why it's false."
+#Prints the string to the screen followed by space.
+puts "How about some more."
+#Prints the string to the screen followed by space.
+puts "Is it greater? #{5.0 > -2.0}"
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+puts "Is it less or equal? #{5.0 <= -2.0}"
+#Prints the strings followed by the calculated floats within the #{}.
