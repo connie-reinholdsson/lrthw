e76ad81e91c23f2c356049a36fc9c4ad7ac89d5e
connie-reinholdsson <connie.reinholdsson@gmail.com>
2017-02-26 15:15:12 +0000

    Ex11 Answers

--- chap11/ex11.rb
+++ chap11/ex11.rb
@@ -1,19 +1,29 @@
 print "How old are you?"
+#prints string.
 age = gets.chomp
-
+#variable age set to user input.
 print "How tall are you?"
+#prints string.
 height = gets.chomp
-
+#variable height set to user input.
 print "How much do you weigh?"
+#prints string.
 weight = gets.chomp
+#variable weight set to user input.
 
 #Note: Print is used instead of puts.
-#Resulting in the answer / input being printed after the question.
-
-#Study Drills Q3
+#Resulting in the answer / input being printed after the question, instead of skipping to the next line.
 
 puts "So you're #{age} years old, #{height} tall and #{weight} heavy."
+#prints string and prints age, height and weight through accessing the variables.
 
+#Study Drills
+#1. gets.chomp takes the input from the user. Note: We must assign gets.chomp to a variable
+#to be able to access the input later in the programme.
+#Gets takes the input from the user and adds a new line
+# and chomp removes the 'enter' character at the end of your string.
+#We use it to get input from the user, e.g. username, passwords, text, messages, status updates, conversations etc.
+#.3
 print "What's your name?"
 name = gets.chomp
 
