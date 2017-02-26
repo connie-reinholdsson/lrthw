418a621a2956c92b246ca6a050408f0d6e4d1c2f
connie-reinholdsson <connie.reinholdsson@gmail.com>
2017-02-26 12:17:42 +0000

    Ex2 Answers

--- chap02/ex2.rb
+++ chap02/ex2.rb
@@ -1,9 +1,12 @@
 # A comment, this is so you can read your program later.
 # Anything after the # is ignored by Ruby.
 
-puts "I could have code like this." # and the comment after is ignored.
+puts "I could have code like this." # and the comment after is ignored
 
 #You can also use a comment to "disable" or comment out a piece of code:
 # puts "This won't run."
 
 puts "This will run."
+
+#Study Drills
+#1. Comments are called 'octothorpe' or 'pound character'.
