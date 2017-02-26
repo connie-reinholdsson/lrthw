1607d5b5c3491264fb5d5414d2ad5dd3e2b7f64f
connie-reinholdsson <connie.reinholdsson@gmail.com>
2017-02-26 15:31:23 +0000

    Ex13a Answers

--- chap13/ex13a.rb
+++ chap13/ex13a.rb
@@ -1,10 +1,10 @@
 first, second, third = ARGV
-#This document takes three arguments.
-
+#ARGV is an 'argument variable'.
+#Line 1 unpacks ARGV, so that it is assigned to three variables: first, second, third.
+#i.e. the document takes three arguments in the terminal.
 puts "Your variable is #{first}."
-
 puts "Your variable is #{second}."
-
 puts "Your variable is #{third}."
 
-#Line 1 unpacks ARGV, so that it is assigned to three variables: first, second, third.
+#Study Drills
+#1. When giving fewer than three arguments it prints the string without the argument variable.
