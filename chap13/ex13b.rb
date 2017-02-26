e15b609ff3299c23b22bd19cc9db2c50f23737fb
connie-reinholdsson <connie.reinholdsson@gmail.com>
2017-02-26 15:31:43 +0000

    Ex13b Answers

--- chap13/ex13b.rb
+++ chap13/ex13b.rb
@@ -1,5 +1,6 @@
 #Study Drills
-name, where_from,   = ARGV
+#2a.
+name, where_from, age, favourite_food  = ARGV
 #One less variable.
 
 puts "Hello #{name}! How are you today?"
@@ -8,6 +9,8 @@ puts "So you are feeling #{answer} today!"
 puts "Is it true that you're from #{where_from}?"
 answer1 = $stdin.gets.chomp
 puts "Wonderful!"
+puts "And you are #{age} years old! You certainly don't look it!"
+puts "And I hear you like #{favourite_food}! YUM!"
 
 #Please note that we need to use '$stdin.gets.chomp' instead of 'gets.chomp' when
 #using ARGV - since ARGV has problems with gets.chomp.
