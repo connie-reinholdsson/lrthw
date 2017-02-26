30144f02af7c48f24e1bb28506f4dfe0d3e31895
connie-reinholdsson <connie.reinholdsson@gmail.com>
2017-02-26 15:52:46 +0000

    Ex15 Answers

--- chap15/ex15.rb
+++ chap15/ex15.rb
@@ -1,15 +1,28 @@
-filename = ARGV.first #Gets the filename.
 
-txt = open(filename)#Use 'open' to open the file.
+filename = ARGV.first #Gets the filename from the user.
+
+txt = open(filename)#Use 'open' to open the file
+#and assigns the content variable 'text'.
 
 puts "Here's your file #{filename}:"
+#prints string and accesses variable 'filename'.
 print txt.read #Use .read method to display the text.
 
+
 print "Type the filename again: "
-file_again = $stdin.gets.chomp #Gets next file.
-txt_again = open(file_again) #Opens the next file.
+#prints string.
+file_again = $stdin.gets.chomp #Gets next file from the user
+#and assigns it to variable file_again.
+txt_again = open(file_again) #Opens the 'file_again'
+#and assigns it to new variable txt_again.
 
-print txt_again.read #Prints the content of the next file.
+print txt_again.read #Prints the content of the 'file_again'.
 
-txt.close
-txt_again.close
+txt.close #Closes the filename.
+txt_again.close #Closes the file_again.
+
+
+#Study Drills
+#4. Prints it once.
+#5. Works fine! Think the second way is better because if the user
+#makes a mistake we can do it again.
