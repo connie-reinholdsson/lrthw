63bebe91b3abc30edc61aebf182878c6f6257323
connie-reinholdsson <connie.reinholdsson@gmail.com>
2017-02-26 13:23:16 +0000

    Ex6 Answers

--- chap06/ex6.rb
+++ chap06/ex6.rb
@@ -1,21 +1,43 @@
 types_of_people = 10
+#Variable types_of_people is 10.
 x = "There are #{types_of_people} types of people."
-binary = "binary"
-do_not = "don't"
+#Variable x is string and accesses variable types_of_people through #{}.
+binary = 'binary'
+#Variable binary is string "binary".
+do_not = "don\'t"
+#Variable do_not is string "don't".
 y = "Those who know #{binary} and those who #{do_not}."
+#Variable y is string and accesses variable binary and variable do_not through #{}.
+puts x
+#Prints variable x with space after.
+puts y
+#Prints variable y with space after.
 
-puts x #"There are 10 types of people."
-puts y #Those who know binary and those who don't.
-
-puts "I said: #{x}." #I said: There are 10 types of people.
-puts " I also said: '#{y}'." #I also said: y..
+puts "I said: #{x}."
+#Prints string and accesses variable x through #{}.
+puts "I also said: '#{y}'."
+#Prints string and accesses variable y through #{}.
 
 hilarious = false
+#Variable hilarious is set to false.
 joke_evaluation = "Isn't that joke so funny? #{hilarious}"
-
-puts joke_evaluation #false
-
+#Variable joke_evaluation is string and accesses variable hilarious through #{}.
+puts joke_evaluation
+#Prints variable joke_evaluation with space after.
 w = 'This is the left side of...'
+#Variable w is string above.
 e = "a string with a right side."
+#Variable e is string above.
+puts w + e
+#Prints variable w and e.
 
-puts w + e #This is the left side of a string with a right side.
+=begin
+Study Drills
+1. Comment on each line.
+2. Find the places where a string is in a string:
+y = "Those who know #{binary} and those who #{do_not}."
+puts "I said: #{x}."
+puts " I also said: '#{y}'."
+4. Because we add two strings together.
+5. 'Don't' doesnt work unless we put in '\'' and we can't access another variable using #{}.
+=end
